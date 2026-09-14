f8ae94eaffcf7a0a4b000e45d614a4c4cf50408e
Luna <luna.pedraza-g@mail.escuelaing.edu.co>
2026-09-13 23:11:04 -0500

    parte practica Luna y Paula

---
+++ build/CMakeFiles/part1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lunam/MCEI_2620/C_C++/parte_practica/part1.cpp" "CMakeFiles/part1.dir/part1.cpp.o" "gcc" "CMakeFiles/part1.dir/part1.cpp.o.d"
+  "" "part1" "gcc" "CMakeFiles/part1.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/part1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/part1.dir/link.d"
+  "CMakeFiles/part1.dir/part1.cpp.o"
+  "CMakeFiles/part1.dir/part1.cpp.o.d"
+  "part1"
+  "part1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/part1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
